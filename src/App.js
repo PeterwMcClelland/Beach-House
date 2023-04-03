84f76e66aa374d7fc33c1690e6bae1d7449cfc3a
Peter McClelland <peterwmcclelland@gmail.com>
2023-04-02 20:48:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Booking from "./components/Booking";
+import Gallery from "./components/Gallery";
+import About from "./components/About";
+import Home from "./components/Home";
+import './App.css';
+
+function App() {
+  return (
+    <>
+            <BrowserRouter>
+                <div className="navbar">
+                    <div id="beachhousenav" className="navlink">
+                        <NavLink to="/" style={({ isActive }) => ({ 
+                            color: isActive ? 'black' : 'black' })}>
+                            Beach House
+                        </NavLink>
+                    </div>
+                    <div className="navlink">
+                        <NavLink to="/gallery" style={({ isActive }) => ({ 
+                            color: isActive ? 'darkgreen' : 'beige' })}>
+                            Gallery
+                        </NavLink>
+                    </div>
+                    <div className="navlink">
+                        <NavLink to="/about" style={({ isActive }) => ({ 
+                            color: isActive ? 'darkgreen' : 'beige' })}>
+                            About
+                        </NavLink>
+                    </div>
+                    <div className="navlink">
+                        <NavLink to="/booking" style={({ isActive }) => ({ 
+                            color: isActive ? 'darkgreen' : 'beige' })}>
+                            
+                        </NavLink>
+                    </div>
+                </div>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/about" element={<About />} />
+                    <Route exact path="/gallery" element={<Gallery />} />
+                    <Route exact path="/booking" element={<Booking />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+  );
+}
+
+export default App;
